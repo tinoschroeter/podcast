54f3dfc3f9b882af388e7f0e97e0b8092a35d10e
Tino Schroeter <tino.schroeter@gmail.com>
2022-08-14 11:39:47 +0200

    remove: console.log

--- backend/libs/getData.js
+++ backend/libs/getData.js
@@ -1,7 +1,6 @@
 const getData = (db) => {
   return (req, res) => {
     const result = db.prepare("select * from podcast").all();
-    console.log(result);
     res.render("index", { message: "", result });
   };
 };
